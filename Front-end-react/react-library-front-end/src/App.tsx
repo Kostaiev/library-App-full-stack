e24992e18864888c2652626bc5be1de1fb33c422
Constantin T <konstantintru99@gmail.com>
2024-02-01 15:30:09 +0200

    Add Okta Authorization

--- Front-end-react/react-library-front-end/src/App.tsx
+++ Front-end-react/react-library-front-end/src/App.tsx
@@ -1,28 +1,57 @@
 import React from "react";
 import "./App.css";
+import { useNavigate } from "react-router-dom";
 import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
 import { Footer } from "./layouts/NavbarAndFooter/Footer";
 import { HomePage } from "./layouts/HomePage/HomePage";
 import { SearchBooksPage } from "./layouts/SearchBooksPage/SearchBooksPage";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { BookCheckoutPage } from "./layouts/BookCheckoutPage/BookCheckoutPage";
+import { oktaConfig } from "./lib/oktaConfig";
+import { OktaAuth, toRelativeUrl } from "@okta/okta-auth-js";
+import { Security, LoginCallback } from "@okta/okta-react";
+import LoginWidget from "./Auth/LoginWidget";
+
+const oktaAuth = new OktaAuth(oktaConfig);
 
 export const App = () => {
+  const history = useNavigate();
+  const customAuthHandler = () => {
+    history("/login");
+  };
+
+  const restoreOriginalUri = async (_oktaAuth: any, originalUri: any) => {
+    history(toRelativeUrl(originalUri || "/", window.location.origin), {
+      replace: true,
+    });
+  };
+
   return (
     <div className="d-flex flex-column min-vh-100">
-      <Navbar />
-      <div className="flex-grow-1">
-        <Routes>
-          <Route path="/" element={<Navigate to="/home" replace />}></Route>
-          <Route path="/home" element={<HomePage />}></Route>
-          <Route path="/search" element={<SearchBooksPage />}></Route>
-          <Route
-            path="/checkout/:bookId"
-            element={<BookCheckoutPage />}
-          ></Route>
-        </Routes>
-      </div>
-      <Footer />
+      <Security
+        oktaAuth={oktaAuth}
+        restoreOriginalUri={restoreOriginalUri}
+        onAuthRequired={customAuthHandler}
+      >
+        <Navbar />
+        <div className="flex-grow-1">
+          <Routes>
+            <Route path="/" element={<Navigate to="/home" replace />}></Route>
+            <Route path="/home" element={<HomePage />}></Route>
+            <Route path="/search" element={<SearchBooksPage />}></Route>
+            <Route
+              path="/checkout/:bookId"
+              element={<BookCheckoutPage />}
+            ></Route>
+            <Route
+              path="/login"
+              element={<LoginWidget config={oktaConfig} />}
+            ></Route>
+            <Route path="/login/collback" element={<LoginCallback />}></Route>
+          </Routes>
+        </div>
+        <Footer />
+      </Security>
     </div>
   );
 };
--- Front-end-react/react-library-front-end/src/layouts/NavbarAndFooter/Navbar.tsx
+++ Front-end-react/react-library-front-end/src/layouts/NavbarAndFooter/Navbar.tsx
@@ -1,6 +1,18 @@
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+import { useOktaAuth } from "@okta/okta-react";
+import { SpinnerLoading } from "../Utils/SpinnerLoading";
 
 export const Navbar = () => {
+  const { oktaAuth, authState } = useOktaAuth();
+
+  if (!authState) {
+    return <SpinnerLoading />;
+  }
+
+  const handelLogout = async () => oktaAuth.signOut();
+
+  console.log(authState);
+
   return (
     <nav className="navbar navbar-expand-lg navbar-dark main-color py-3">
       <div className="container-fluid">
@@ -42,11 +54,26 @@ export const Navbar = () => {
             </li>
           </ul>
           <ul className="navbar-nav ms-auto">
-            <li className="nav-item m-1">
-              <a type="button" className="btn btn-outline-light" href="/login">
-                Sign in
-              </a>
-            </li>
+            {!authState.isAuthenticated ? (
+              <li className="nav-item m-1">
+                <Link
+                  type="button"
+                  className="btn btn-outline-light"
+                  to="/login"
+                >
+                  Sign in
+                </Link>
+              </li>
+            ) : (
+              <li>
+                <button
+                  className="btn btn-outline-light"
+                  onClick={handelLogout}
+                >
+                  Log out
+                </button>
+              </li>
+            )}
           </ul>
         </div>
       </div>
